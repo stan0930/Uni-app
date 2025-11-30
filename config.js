d9afd85c958f6577a7fd9f3032530d6d1e314f6a
Luowenting <3087615590@qq.com>
2025-11-30 21:12:23 +0800

    改错
    baseurl记得换

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // 应用全局配置
 module.exports = {
   // baseUrl: 'https://vue.ruoyi.vip/prod-api',
-  // 使用代理路径，彻底解决跨域问题
-  baseUrl: '/dev-api',
+  baseUrl: 'http://192.168.135.204:8080',
+  // baseUrl: '/dev-api',
   // 应用信息
   appInfo: {
     // 应用名称
